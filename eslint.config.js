b5e286ccfd411780a0ffb85efc4d8bdd7782c30d
Jahongir <nusratovjahongir@gmail.com>
2025-07-10 12:17:25 +0500

    feat: setup eslint with prettier.

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,36 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import { globalIgnores } from 'eslint/config';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+// ✅ import installed plugins
+import eslintPluginPrettier from 'eslint-plugin-prettier';
+import eslintPluginUnicorn from 'eslint-plugin-unicorn';
 
 export default tseslint.config([
   globalIgnores(['dist']),
   {
     files: ['**/*.{ts,tsx}'],
+    // ✅ eslint-plugin-prettier like this
+    plugins: {
+      prettier: eslintPluginPrettier,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      quotes: ['error', 'single'],
+    },
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
+      // ✅ eslint-plugin-unicorn here
+      eslintPluginUnicorn.configs.recommended,
     ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
   },
-])
+]);
